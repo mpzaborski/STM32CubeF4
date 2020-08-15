aab51e643339e3b5377d69b490b7437d1783f0d2
Mateusz Zaborski <mateusz.zaborski@assaabloy.com>
2020-08-16 01:54:39 +0200

    bme280 moved to separate file
    fixes

--- Projects/STM32F401RE-Nucleo/Templates/Src/main.c
+++ Projects/STM32F401RE-Nucleo/Templates/Src/main.c
@@ -37,6 +37,7 @@
 #include "main.h"
 #include "ssd1306.h"
 #include "bme280.h"
+#include "bme280_port.h"
 #include "string.h"
 #include <stdio.h>
 
@@ -55,22 +56,23 @@
 #define I2C_SLAVE_ADDRESS 0x30
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-UART_HandleTypeDef UartHandle;
 I2C_HandleTypeDef I2cHandle;
 TIM_HandleTypeDef TimHandle;
-struct bme280_dev bme_dev;
-
-volatile uint8_t displayActive = 0;
-
+static UART_HandleTypeDef UartHandle;
+static struct bme280_dev bme_dev;
+static volatile uint8_t displayActive = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Error_Handler(void);
+static void UartInit(void);
+static void I2cInit(void);
+static void TimerInit(void);
 static void EXTILine8_Config(void);
+static void ssd1306_print_measurements(int32_t temperature, uint32_t humidity, uint32_t pressure);
 
 /* Private functions ---------------------------------------------------------*/
-
-void UartInit(void)
+static void UartInit(void)
 {
   UartHandle.Instance        = USART2;
   UartHandle.Init.BaudRate   = 115200;
@@ -84,7 +86,7 @@ void UartInit(void)
   HAL_UART_Init(&UartHandle);
 }
 
-void I2cInit(void)
+static void I2cInit(void)
 {
   /*##-1- Configure the I2C peripheral ######################################*/
   I2cHandle.Instance             = I2Cx;
@@ -104,88 +106,15 @@ void I2cInit(void)
   }
 }
 
-int8_t user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
-{
-  uint8_t dev_addr;
-  dev_addr = BME280_I2C_ADDR_PRIM;
-
-  HAL_StatusTypeDef status = HAL_OK;
-  int32_t iError = 0;
-  uint8_t array[40] = {0};
-  uint8_t stringpos = 0;
-  array[0] = reg_addr;
-
-  while (HAL_I2C_IsDeviceReady(&I2cHandle, (uint16_t)(dev_addr<<1), 3, 100) != HAL_OK) {}
-
-  status = HAL_I2C_Mem_Read(&I2cHandle,	  // i2c handle
-              (uint8_t)(dev_addr<<1), // i2c address, left aligned
-              (uint8_t)reg_addr,  // register address
-              I2C_MEMADD_SIZE_8BIT,			// bme280 uses 8bit register addresses
-              (uint8_t*)(array),			// write returned data to this variable
-              len,							// how many bytes to expect returned
-              HAL_MAX_DELAY);							// timeout
-
-    //while (HAL_I2C_IsDeviceReady(&I2cHandle, (uint8_t)(id.dev_addr<<1), 3, 100) != HAL_OK) {}
-
-    if (status != HAL_OK)
-    {
-      // The BME280 API calls for 0 return value as a success, and -1 returned as failure
-      iError = (-1);
-    }
-  for (stringpos = 0; stringpos < len; stringpos++) {
-    *(reg_data + stringpos) = array[stringpos];
-  }
-
-  return (int8_t)iError;
-}
-
-int8_t user_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
-{
-  uint8_t dev_addr;
-  dev_addr = BME280_I2C_ADDR_PRIM;
-
-    HAL_StatusTypeDef status = HAL_OK;
-  int32_t iError = 0;
-
-  //while (HAL_I2C_IsDeviceReady(&I2cHandle, (uint8_t)(dev_addr<<1), 3, 100) != HAL_OK) {}
-
-    status = HAL_I2C_Mem_Write(&I2cHandle,						// i2c handle
-                (uint8_t)(dev_addr<<1),		// i2c address, left aligned
-                (uint8_t)reg_addr,			// register address
-                I2C_MEMADD_SIZE_8BIT,			// bme280 uses 8bit register addresses
-                (uint8_t*)(reg_data),		// write returned data to reg_data
-                len,							// write how many bytes
-                HAL_MAX_DELAY);							// timeout
-
-  if (status != HAL_OK)
-    {
-        // The BME280 API calls for 0 return value as a success, and -1 returned as failure
-      iError = (-1);
-    }
-  return (int8_t)iError;
-}
-
-void user_delay_us(uint32_t period, void *intf_ptr)
-{
-	HAL_Delay(period/1000);
-}
-
-void TimerInit(void)
+static void TimerInit(void)
 {
   uint32_t uwPrescalerValue;
-  /* Compute the prescaler value to have TIMx counter clock equal to 10000 Hz */
-  uwPrescalerValue = (uint32_t)(SystemCoreClock / 10000) - 1;
+  /* Compute the prescaler value to have TIMx counter clock equal to 1000 Hz */
+  uwPrescalerValue = (uint32_t)((SystemCoreClock / 2) / 1000) - 1;
 
-  /* Set TIMx instance */
+  /* Don't know why, but in this configuration it ticks every 10 seconds (period should be 10000) */
   TimHandle.Instance = TIMx;
-
-  /* Initialize TIMx peripheral as follows:
-       + Period = 50000 - 1
-       + Prescaler = ((SystemCoreClock / 2)/10000) - 1
-       + ClockDivision = 0
-       + Counter direction = Up
-  */
-  TimHandle.Init.Period            = 50000 - 1;
+  TimHandle.Init.Period            = 20000 - 1;
   TimHandle.Init.Prescaler         = uwPrescalerValue;
   TimHandle.Init.ClockDivision     = 0;
   TimHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
@@ -198,90 +127,25 @@ void TimerInit(void)
   }
 }
 
-int8_t bme280_sensor_init(struct bme280_dev  *dev)
+/**
+  * @brief  Configures EXTI Line8 (connected to PA8 pin) in interrupt mode
+  * @param  None
+  * @retval None
+  */
+static void EXTILine8_Config(void)
 {
-  int8_t rslt = BME280_OK;
-  uint8_t dev_addr = BME280_I2C_ADDR_PRIM;
-  uint8_t sensor_mode;
+  GPIO_InitTypeDef   GPIO_InitStructure;
 
-  dev->intf_ptr = &dev_addr;
-  dev->intf = BME280_I2C_INTF;
-  dev->read = user_i2c_read;
-  dev->write = user_i2c_write;
-  dev->delay_us = user_delay_us;
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  rslt = bme280_init(dev);
-  bme280_get_sensor_mode(&sensor_mode, dev);
-  return rslt;
-}
+  GPIO_InitStructure.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStructure.Pin = GPIO_PIN_6;
+  GPIO_InitStructure.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6, GPIO_PIN_SET);
 
-int8_t stream_sensor_data_normal_mode(struct bme280_dev *dev)
-{
-  int8_t rslt;
-  uint8_t settings_sel;
-
-  /* Recommended mode of operation: Indoor navigation */
-  dev->settings.osr_h = BME280_OVERSAMPLING_1X;
-  dev->settings.osr_p = BME280_OVERSAMPLING_16X;
-  dev->settings.osr_t = BME280_OVERSAMPLING_2X;
-  dev->settings.filter = BME280_FILTER_COEFF_16;
-  dev->settings.standby_time = BME280_STANDBY_TIME_62_5_MS;
-
-  settings_sel = BME280_OSR_PRESS_SEL;
-  settings_sel |= BME280_OSR_TEMP_SEL;
-  settings_sel |= BME280_OSR_HUM_SEL;
-  settings_sel |= BME280_STANDBY_SEL;
-  settings_sel |= BME280_FILTER_SEL;
-  rslt = bme280_set_sensor_settings(settings_sel, dev);
-  rslt = bme280_set_sensor_mode(BME280_NORMAL_MODE, dev);
-
-  return rslt;
-}
-
-#define POSITIVE_TEMPERATURE_RANGE 0x7FF
-
-#define MCP9808_TEMPERATURE_AMBIENT 0x05
-#define MCP9808_MANUFACTURER_ID 0x06
-
-// Example of temperature conversion. Potentially some errors there (i.e negative temperature)
-// https://www.digikey.pl/en/maker/projects/getting-started-with-stm32-i2c-example/ba8c2bfef2024654b5dd10012425fa23
-
-void mcp9808_read_temperature(char* result_buffer)
-{
-  uint16_t temperature;
-  float f_temperature;
-  HAL_I2C_Mem_Read(&I2cHandle, I2C_SLAVE_ADDRESS, MCP9808_TEMPERATURE_AMBIENT, 1, (uint8_t*)&temperature, sizeof(temperature), HAL_MAX_DELAY);
-  /* swap beacuse of endianess */
-  temperature = __REV16(temperature);
-  /* ignore alert pin state bits */
-  temperature = temperature & 0x1fff;
-  if ( temperature > POSITIVE_TEMPERATURE_RANGE)
-  {
-    temperature = ~temperature;
-    temperature+=1;
-    temperature = temperature & 0x7ff;
-    f_temperature = temperature * 0.0625;
-    f_temperature *= 100;
-    sprintf(result_buffer, "-%d.%d C\n\r", (int)f_temperature / 100, ((int) f_temperature % 100)/10);
-  }
-  else
-  {
-    temperature = temperature & 0x7ff;
-    f_temperature = temperature * 0.0625;
-    f_temperature *= 100;
-    sprintf(result_buffer, "%d.%d C\n\r", (int)f_temperature / 100, ((int) f_temperature % 100)/10);
-  }
-}
-
-uint16_t mcp9808_read_manufacturer_id(char* buffer)
-{
-  uint16_t manufacturer_id=0xabcd;
-
-  HAL_I2C_Mem_Read(&I2cHandle, I2C_SLAVE_ADDRESS, MCP9808_MANUFACTURER_ID, 1, (uint8_t*)&manufacturer_id, sizeof(manufacturer_id), HAL_MAX_DELAY);
-
-  manufacturer_id = __REV16(manufacturer_id);
-  sprintf(buffer, "manufacturer id: 0x%02x\r\n", manufacturer_id);
-  return manufacturer_id;
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 }
 
 void ssd1306_print_measurements(int32_t temperature, uint32_t humidity, uint32_t pressure)
@@ -290,14 +154,14 @@ void ssd1306_print_measurements(int32_t temperature, uint32_t humidity, uint32_t
 
   ssd1306_Fill(White);
   ssd1306_SetCursor(2, 0);
-  sprintf(buffer, "%d.%d C\n\r",temperature / 100, (temperature % 100) / 10);
+  sprintf(buffer, "%ld.%ld C\n\r",temperature / 100, (temperature % 100) / 10);
   ssd1306_WriteString(buffer, Font_11x18, Black);
   ssd1306_DrawCircle(54,3,2,Black);
   ssd1306_SetCursor(2,18);
-  sprintf(buffer, "%d.%d %%\n\r", humidity /1024, (humidity % 1024) / 102);
+  sprintf(buffer, "%ld.%ld %%\n\r", humidity /1024, (humidity % 1024) / 102);
   ssd1306_WriteString(buffer, Font_11x18, Black);
   ssd1306_SetCursor(2,36);
-  sprintf(buffer, "%d.%1.d hPa\n\r",pressure /100, (pressure % 100) / 10);
+  sprintf(buffer, "%ld.%ld hPa\n\r",pressure /100, (pressure % 100) / 10);
   ssd1306_WriteString(buffer, Font_11x18, Black);
 
   ssd1306_UpdateScreen();
@@ -322,7 +186,7 @@ int main(void)
      */
   HAL_Init();
 
-  /* Configure the System clock to have a frequency of 216 MHz */
+  /* Configure the System clock to have a frequency of 84 MHz */
   SystemClock_Config();
 
   /* -1- Enable GPIOA Clock (to be able to program the configuration registers) */
@@ -332,48 +196,24 @@ int main(void)
   UartInit();
   I2cInit();
   TimerInit();
-  bme280_sensor_init(&bme_dev);
+  bme280_port_sensor_init(&bme_dev);
 
   struct bme280_data bme280_d;
 
   ssd1306_Init();
   ssd1306_WriteCommand(0xAE); //display off
 
-  stream_sensor_data_normal_mode(&bme_dev);
-
   while(1)
   {
 	bme280_get_sensor_data(BME280_ALL, &bme280_d, &bme_dev);
     if(displayActive)
     {
-      sprintf(buffer, "T:%d, H:%d P:%d\n\r",bme280_d.temperature, bme280_d.humidity, bme280_d.pressure);
+      sprintf(buffer, "T:%ld, H:%ld P:%ld\n\r",bme280_d.temperature, bme280_d.humidity, bme280_d.pressure);
       HAL_UART_Transmit(&UartHandle, (uint8_t*)buffer, strlen(buffer), 10);
       ssd1306_print_measurements(bme280_d.temperature, bme280_d.humidity, bme280_d.pressure);
     }
     HAL_Delay(1000);
   }
-}
-
-
-/**
-  * @brief  Configures EXTI Line8 (connected to PA8 pin) in interrupt mode
-  * @param  None
-  * @retval None
-  */
-static void EXTILine8_Config(void)
-{
-  GPIO_InitTypeDef   GPIO_InitStructure;
-
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  GPIO_InitStructure.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStructure.Pin = GPIO_PIN_6;
-  GPIO_InitStructure.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
-  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6, GPIO_PIN_SET);
-
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
